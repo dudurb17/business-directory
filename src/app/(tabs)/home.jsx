8d082a97e6f98c366fda0523562e1e2c6fa45c19
dudurb17 <eduardo.robettibedin@gmail.com>
2024-11-25 18:30:36 -0300

    created header component

--- src/app/(tabs)/home.jsx
+++ src/app/(tabs)/home.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Header from "../../components/Home/Header";
 
 export default function Home() {
   return (
     <View>
-      <Text style={{ fontSize: 40, fontFamily: "outfit-bold" }}>Home</Text>
+      <Header />
     </View>
   );
 }
