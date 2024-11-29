ec035e9769247026d4da4d8dc8a6508d4feb638c
dudurb17 <eduardo.robettibedin@gmail.com>
2024-11-29 11:03:33 -0300

    created page profile

--- src/app/(tabs)/profile.jsx
+++ src/app/(tabs)/profile.jsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import UserIntro from "../../components/Profile/UserIntro";
+import MenuList from "../../components/Profile/MenuList";
 
 export default function Profile() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View
+      style={{
+        padding: 20,
+      }}
+    >
+      <Text style={{ fontFamily: "outfit-bold", fontSize: 35 }}> Profile</Text>
+      <UserIntro />
+      <MenuList />
     </View>
-  )
+  );
 }
